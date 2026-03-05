8f1bafef0e7f9fc2027e4ceab5016d31eb4aaf88
Saksham-Xtreme <shivbhau2108@gmail.com>
2026-03-06 03:23:51 +0530

    fix: use environment variables for MongoDB and sessions

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const listingRoute = require("./routes/listing.js");
 const reviewRoute = require("./routes/review.js");
 const userRoute = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/AirBnb";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/AirBnb";
 
 // DB + Server
 
